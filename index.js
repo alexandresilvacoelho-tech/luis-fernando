1537fb40494b741989c28d1d69d13956fb5b7a0b
alexandresilvacoelho-tech <alexandre.silva.coelho@escola.pr.gov.br>
2026-05-12 11:57:20 -0300

    abner

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ const frutas = ["banana", "maçã", "mamão", "melão", "pitaya"];
 console.log ("listas", frutas);
 console.log ("primeira:", frutas [0]);
 console.log ("ultima:", frutas [frutas.length - 1])
+
+//lista de numeros inteiros
+const numeros = ["1", "2", "3", "4", "5", "6"];
+console.log (S)
