77cf0641f27fa61d145fde36a0cf777d85db7dc4
alexandresilvacoelho-tech <alexandre.silva.coelho@escola.pr.gov.br>
2026-05-12 11:52:20 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# luis-fernando
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/alexandresilvacoelho-tech/luis-fernando)
---
+++ index.js
@@ -0,0 +1,5 @@
+//1. listas de frutas
+const frutas = ["banana", "maçã", "mamão", "melão", "pitaya"];
+console.log ("listas", frutas);
+console.log ("primeira:", frutas [0]);
+console.log ("ultima:", frutas [frutas.length - 1])
